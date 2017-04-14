6c73ab817c6f95f1c29cced6f4f2cf4da72e3786
TobiahRex <tobiah.rex@gmail.com>
2017-04-14 11:06:03 +0900

    finished execercise 26.

--- index.js
+++ index.js
@@ -709,7 +709,7 @@ function exc20() {
 )
 )
 }
-// console.log(exc20());
+// console.log('excercise 20: \n', exc20());
 // -----------------------------------------------------------------------------
 /*
 Zipping Arrays
@@ -1038,17 +1038,136 @@ function exc25() {
 	// Their Answer
 	// 1) Whenever you see that you're potentially deleting a property on every object, consider using a map and only returning keys that you desire.
 	return lists.map(({ name, id }) => ({
-			name,
-			videos: videos
-			.filter(({ listId }) => listId === id)
-			.map(({ id, title }) => ({ id, title }))
-		})
-	);
+		name,
+		videos: videos
+		.filter(({ listId }) => listId === id)
+		.map(({ id, title }) => ({ id, title }))
+	})
+);
 }
 // console.log(exc25());
 // -----------------------------------------------------------------------------
+/*
 Looks like you figured out that you can use map and filter to join two different arrays by a key. Now let's try a more complex example...
 
 Exercise 26: Converting from Arrays to Deeper Trees
 
 Let's try creating a deeper tree structure. This time we have 4 separate arrays each containing lists, videos, boxarts, and bookmarks respectively. Each object has a parent id, indicating its parent. We want to build an array of list objects, each with a name and a videos array. The videos array will contain the video's id, title, bookmark time, and smallest boxart url. In other words we want to build the following structure:
+
+[
+{
+"name": "New Releases",
+"videos": [
+{
+"id": 65432445,
+"title": "The Chamber",
+"time": 32432,
+"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg"
+},
+{
+"id": 675465,
+"title": "Fracture",
+"time": 3534543,
+"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture120.jpg"
+}
+]
+},
+{
+"name": "Thrillers",
+"videos": [
+{
+"id": 70111470,
+"title": "Die Hard",
+"time": 645243,
+"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+},
+{
+"id": 654356453,
+"title": "Bad Boys",
+"time": 984934,
+"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg"
+}
+]
+}
+]
+*/
+function exc26() {
+	var lists = [
+		{
+			"id": 5434364,
+			"name": "New Releases"
+		},
+		{
+			"id": 65456475,
+			name: "Thrillers"
+		}
+	],
+	videos = [
+		{
+			"listId": 5434364,
+			"id": 65432445,
+			"title": "The Chamber"
+		},
+		{
+			"listId": 5434364,
+			"id": 675465,
+			"title": "Fracture"
+		},
+		{
+			"listId": 65456475,
+			"id": 70111470,
+			"title": "Die Hard"
+		},
+		{
+			"listId": 65456475,
+			"id": 654356453,
+			"title": "Bad Boys"
+		}
+	],
+	boxarts = [
+		{
+			videoId: 65432445,
+			width: 130,
+			height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg"
+		},
+		{ videoId: 65432445, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
+		{ videoId: 675465, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+		{ videoId: 675465, width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+		{ videoId: 675465, width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+		{ videoId: 70111470, width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+		{ videoId: 70111470, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
+		{ videoId: 654356453, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+		{ videoId: 654356453, width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+	],
+	bookmarks = [
+		{ videoId: 65432445, time: 32432 },
+		{ videoId: 675465, time: 3534543 },
+		{ videoId: 70111470, time: 645243 },
+		{ videoId: 654356453, time: 984934 }
+	];
+
+	return lists.map(({ id, name }) => ({
+		name,
+		videos: videos
+		.filter(({ listId }) => listId === id)
+		.concatMap((video) =>
+			Array.zip(
+				bookmarks
+				.filter(({ videoId }) => videoId === video.id),
+
+				boxarts
+				.filter(({ videoId }) => videoId === video.id)
+				.reduce((first, next) => {
+					if ((first.width * first.height) <  (next.width * next.height)) return first.url;
+					return next.url;
+				}), ({ time }, url) => ({
+					id: video.id,
+					title: video.title,
+					time,
+					boxart: url,
+				})
+			)
+		)
+	}))
+}
+console.log('excercise 26: \n', JSON.stringify(exc26(), 0, 2));
